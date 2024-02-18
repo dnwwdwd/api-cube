861511d40d41a6be8ed11da0f3592ff82686a980
C1own <1765393959@qq.com>
2024-02-18 21:54:27 +0800

    add 接口信息增删改查 add API签名认证和自定义开发SDK add 上线/下线接口 add 在线调用 add 引入Gateway网关

---
+++ apicube-gateway/src/main/java/com/hjj/apicubegateway/ApicubeGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.hjj.apicubegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApicubeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApicubeGatewayApplication.class, args);
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("tohomiematching", r -> r.path("/homiematching")
+                        .uri("http://47.113.227.23"))
+                .route("toyupiicu", r -> r.path("/yupi")
+                        .uri("https://yupi.icu"))
+                .build();
+    }
+}
---
+++ apicube-gateway/src/test/java/com/hjj/apicubegateway/ApicubeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjj.apicubegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicubeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
