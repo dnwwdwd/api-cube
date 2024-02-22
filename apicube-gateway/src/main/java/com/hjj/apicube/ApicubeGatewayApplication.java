fa80de6ca87df112729306889aeeb203a6563fa9
C1own <1765393959@qq.com>
2024-02-22 20:35:16 +0800

    add 接口信息增删改查 add API签名认证和自定义开发SDK add 上线/下线接口 add 在线调用 add 引入Gateway网关 add Gateway配置和初使用 add 初步引入Dubbo RPC调用invokeCount

---
+++ apicube-gateway/src/main/java/com/hjj/apicube/ApicubeGatewayApplication.java
@@ -0,0 +1,45 @@
+package com.hjj.apicube;
+
+import com.hjj.apicube.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+@EnableDubbo
+@Service
+public class ApicubeGatewayApplication {
+
+    @DubboReference
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(ApicubeGatewayApplication.class, args);
+        ApicubeGatewayApplication application = context.getBean(ApicubeGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
+    }
+
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
+    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("tohomiematching", r -> r.path("/homiematching")
+//                        .uri("http://47.113.227.23"))
+//                .route("toyupiicu", r -> r.path("/yupi")
+//                        .uri("https://yupi.icu"))
+//                .build();
+//    }
+}
---
+++ apicube-gateway/src/main/java/com/hjj/apicube/provider/DemoService.java
@@ -0,0 +1,20 @@
+package com.hjj.apicube.provider;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * 示例服务
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+    String sayHello2(String name);
+
+    default CompletableFuture<String> sayHelloAsync(String name) {
+        return CompletableFuture.completedFuture(sayHello(name));
+    }
+}
